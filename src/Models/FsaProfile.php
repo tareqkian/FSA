6041e671b579d5c47a7229182f55c87b9b5ca9d6
tareqkian <tarekian99@gmail.com>
2023-07-15 03:19:08 +0300

    development

--- src/Models/FsaProfile.php
+++ src/Models/FsaProfile.php
@@ -1,10 +1,31 @@
 <?php
 
-namespace Tarek\Fsa\Models;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class FsaProfile extends Model
 {
-  //
+
+  protected $primaryKey = 'user_id';
+
+  protected $fillable = [
+    "user_id",
+    "first_name",
+    "last_name",
+    "birthday",
+    "gender",
+    "address",
+    "city",
+    "state",
+    "postal_code",
+    "country",
+    "locale"
+  ];
+
+  public function user(): BelongsTo
+  {
+    return $this->belongsTo(User::class, 'user_id', 'id');
+  }
 }
